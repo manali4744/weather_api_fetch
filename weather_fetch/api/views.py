147c595d1ce7a3da589d97458464430677e485d6
manali4744 <manali.empiric98@gmail.com>
2023-04-20 15:22:27 +0530

    env

--- weather_fetch/api/views.py
+++ weather_fetch/api/views.py
@@ -10,16 +10,14 @@ import os
 # Create your views here.
 def get_data_weather(data):
     BASE_URL = "https://demo.openweathermap.org/energy/1.0/solar/data?"
-    
     API_KEY = os.environ['API_KEY']
     LAT = str(data['lat'])
     LONG = str(data['long'])
     DATE = str(data['Date'])
     url = BASE_URL + "lat=" + LAT + "&lon=" + LONG + "&date=" + DATE + "&appid=" + API_KEY
     reaponse = requests.get(url).json()
-    print(reaponse['irradiance']['daily'])
+    # print(reaponse['irradiance']['daily'])
     return reaponse
-
 
 
 class WeatherView(APIView):
@@ -28,4 +26,4 @@ class WeatherView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             data = get_data_weather(data=serializer.data)
-            return Response({'data':data['irradiance']['daily'], 'status': status.HTTP_200_OK})
+            return Response({'data':data['irradiance']['daily'], 'status': status.HTTP_200_OK, 'info': data})
